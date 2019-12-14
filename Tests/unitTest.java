f3dce1cea57c137665c8d795dcf355c88991ae24
landry <landry.mpati@gmail.com>
2019-12-14 19:29:50 +0100

    Unit test

--- Tests/unitTest.java
+++ Tests/unitTest.java
@@ -1,9 +1,7 @@
 import domaine.Account;
 import org.junit.jupiter.api.Test;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class unitTest {
 
