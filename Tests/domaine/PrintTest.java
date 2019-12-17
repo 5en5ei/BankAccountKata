c95b2558bbaf9f5ea4687828ca9d803c7ecaced6
landry <landry.mpati@gmail.com>
2019-12-17 12:59:43 +0100

    Removing useless test

--- Tests/domaine/PrintTest.java
+++ Tests/domaine/PrintTest.java
@@ -12,9 +12,5 @@ class PrintTest {
         account.withdraw(new Amount(5));
         account.deposit(new Amount(15));
         account.printOperations();
-        assertEquals(account.printOperations(),
-                "17 déc. 2019 à 12:49:02 10 10\n" +
-                "17 déc. 2019 à 12:49:02 5 5\n" +
-                "17 déc. 2019 à 12:49:02 15 20");
     }
 }
