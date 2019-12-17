4242fd2a9fac36554063de2ebd9bd0c00f8d88a1
landry <landry.mpati@gmail.com>
2019-12-17 17:36:38 +0100

    Clearing useless imports

--- Tests/domaine/PrintTest.java
+++ Tests/domaine/PrintTest.java
@@ -6,7 +6,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class PrintTest {
