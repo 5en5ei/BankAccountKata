52b878e78ff3598d34639e59a37b9ecd650a98a5
landry <landry.mpati@gmail.com>
2019-12-17 11:01:16 +0100

    Adding some missing unit tests

--- Tests/domaine/AmountTest.java
+++ Tests/domaine/AmountTest.java
@@ -8,13 +8,19 @@ class AmountTest {
 
     @Test
     void isNegative() {
+        Amount amount = new Amount(-10);
+        assertTrue(amount.isNegative());
     }
 
     @Test
     void addMoney() {
+        Amount amount = new Amount(10);
+        assertEquals(amount.addMoney(new Amount(10)).getAmount(), 20);
     }
 
     @Test
     void retrieveMoney() {
+        Amount amount = new Amount(10);
+        assertEquals(amount.retrieveMoney(new Amount(10)).getAmount(), 0);
     }
 }
